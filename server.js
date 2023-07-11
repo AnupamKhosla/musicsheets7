d5cc9df749db12a2b368930bc5ac0bb758656c7d
Anon <anupamkhosla1993@gmail.com>
2023-07-12 02:36:17 +0530

    debugging git push without eb-deploy

--- server.js
+++ server.js
@@ -9,8 +9,9 @@ import path from 'node:path';
 import cors from "cors";
 import "./loadEnvironment.mjs";
 import "express-async-errors";
+import posts from "./routes/posts.mjs";
 import db from "./db/conn.mjs";
-console.log(db);
+
 
 
 
@@ -25,45 +26,19 @@ publicDir = path.join(path.resolve(),'public');
 app.use(cors());
 app.use(express.static(publicDir));
 
-app.use("/latest", async (req, res) => {
-  let collection = await db.collection("posts");
-  let results = await collection.aggregate([
-    {"$project": {"author": 1, "title": 1, "tags": 1, "date": 1}},
-    {"$sort": {"date": -1}},
-    {"$limit": 3}
-  ]).toArray();
-  res.send(results).status(200);
-});
+//app.use(express.json());
 
+// Load the /posts routes
+app.use("/posts", posts);
 
+// Global error handling
 app.use((err, _req, res, next) => {
   res.status(500).send("Uh oh! An unexpected error occured.")
 })
 
 // start the Express server
 app.listen(PORT, () => {
-  console.log(`Server is running on port: ${PORT}`);
+  console.log(`Success! Server is running on port: ${PORT}`);
 });
 
-// app.listen(port);
-// console.log("listen started");
-
-//module.exports = app;
 export default app;
-
-///////////////////
-
-// app.use(express.json());
-
-// // Load the /posts routes
-// app.use("/posts", posts);
-
-// // Global error handling
-// app.use((err, _req, res, next) => {
-//   res.status(500).send("Uh oh! An unexpected error occured.")
-// })
-
-// // start the Express server
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port: ${PORT}`);
-// });
